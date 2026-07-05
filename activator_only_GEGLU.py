33f78290c37c955f8a876ec7565928a97ca637db
Abdullah Nazhat Abdullah <abdullah.nazhat@gmail.com>
2026-07-06 01:32:42 +0300

    Clean up code formatting and remove blank lines
    Removed unnecessary blank lines and fixed formatting in class initializers.

--- activator_only_GEGLU.py
+++ activator_only_GEGLU.py
@@ -1,11 +1,6 @@
 import torch
 from torch import nn
 
-
-
-
-   
-
 class ActivatorGatingUnit(nn.Module):
     def __init__(self,dim, hidden_dim):
         super().__init__()
@@ -29,15 +24,13 @@ class ActivatorGatingUnit(nn.Module):
         out = self.proj_3(g)
         return out
 
-
-
 class ActivatorBlock(nn.Module):
-    def __init__(self, d_model, d_ffn,dropout):
+    def __init__(self, d_model, d_ffn, dropout):
         super().__init__()
        
         self.norm = nn.LayerNorm(d_model)       
         self.actgu = ActivatorGatingUnit(d_model, d_ffn)
-        #self.ffn = FeedForward(d_model,d_ffn,dropout)
+        #self.ffn = FeedForward(d_model, d_ffn, dropout)
     def forward(self, x):
         residual = x
         x = self.norm(x)
@@ -47,24 +40,14 @@ class ActivatorBlock(nn.Module):
         out = x
         return out
 
-
-
 class ACTIVATOR(nn.Module):
-    def __init__(self, d_model, d_ffn, num_layers,dropout):
+    def __init__(self, d_model, d_ffn, num_layers, dropout):
         super().__init__()
         
         self.model = nn.Sequential(
-            *[ActivatorBlock(d_model,d_ffn,dropout) for _ in range(num_layers)]
+            *[ActivatorBlock(d_model, d_ffn, dropout) for _ in range(num_layers)]
         )
 
     def forward(self, x):
        
         return self.model(x)
-
-
-
-
-
-
-
-
